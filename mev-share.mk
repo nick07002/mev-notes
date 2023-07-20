20a5a63374565d34bb6a509eb17d3c67f86297a6
Nick <113782315+nick07002@users.noreply.github.com>
2023-07-19 20:23:17 -0700

    Create mev-share.mk

---
+++ mev-share.mk
@@ -0,0 +1,29 @@
+# MEV-Share Resources
+
+This document provides a collection of resources related to MEV-Share, including documentation, code examples, video tutorials, and testnet APIs.
+
+## Documentation
+
+The official documentation for MEV-Share can be found on the Flashbots website. It provides a comprehensive overview of the MEV-Share RPC.
+
+- [MEV-Share Documentation](https://docs.flashbots.net/flashbots-protect/rpc/mev-share)
+
+## Code Examples
+
+Flashbots provides a simple blind arbitrage example on their GitHub. This can be a good starting point for understanding how to implement MEV-Share in a project.
+
+- [Simple Blind Arbitrage Example](https://github.com/flashbots/simple-blind-arbitrage)
+
+
+## Video Tutorial
+
+Flashbots has also created a video tutorial that walks through the process of using MEV-Share. This can be a helpful resource for visual learners.
+
+- [MEV-Share Video Tutorial](https://www.youtube.com/watch?v=Twcr2vZ1tRU&ab_channel=Flashbots)
+
+## Testnet APIs
+
+MEV-Share has a deployment on the Goerli testnet. The following links provide access to the SSE and Relay for this deployment.
+
+- SSE: [https://mev-share-goerli.flashbots.net](https://mev-share-goerli.flashbots.net)
+- Relay: [https://relay-goerli.flashbots.net](https://relay-goerli.flashbots.net)
